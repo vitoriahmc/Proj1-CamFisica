f2189d585eec7a41e1ee8aa648fe0085109782f0
SabrinaSimao <sabrina_simao@hotmail.com>
2017-08-11 15:17:02 -0300

    client

---
+++ 0-COM-LoopBack/src/client.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+# Prof. Rafael Corsi
+#  Abril/2017
+#  Client
+####################################################
+
+from enlace import *
+#import time
+
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM6"                  # Windows(variacao de)
+
+def main():
+    # Inicializa enlace
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    # Endereco da imagem a ser transmitida
+    imageR = "./imgs/imageC.png"
+
+    # Log
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(com.fisica.name))
+    print("-------------------------")
+
+    # Carrega imagem
+    print ("Carregando imagem para transmissão :")
+    print (" - {}".format(imageR))
+    print("-------------------------")
+    txBuffer = open(imageR, 'rb').read()
+    txLen    = len(txBuffer)
+    print(txLen)
+
+    # Transmite imagem
+    print("Transmitindo .... {} bytes".format(txLen))
+    com.sendData(txBuffer)
+
+    # espera o fim da transmissão
+    while(com.tx.getIsBussy()):
+        pass
+
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+    print ("Transmitido       {} bytes ".format(txSize))
+
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+
+    # Fecha arquivo de imagem
+    f.close()
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+if __name__ == "__main__":
+    main()
